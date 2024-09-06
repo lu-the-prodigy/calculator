bd1150f8e464899afab4e19ecac67d83067c9f05
AKhasanov <hasanovasad12@gmail.com>
2024-09-06 18:19:11 +0500

    initial

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,17 +1,74 @@
 package com.company;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите выражение : ");
+        String input = scanner.nextLine();
+        try {
+            String result = calc(input);
+            System.out.println("Результат : " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public static String calc(String input) throws Exception {
+
+        input = input.trim();
+
+        char operator = ' ';
+
+        if (input.contains(" + ")) {
+            operator = '+';
+        } else if (input.contains(" - ")) {
+            operator = '-';
+        } else if (input.contains(" * ")) {
+            operator = '*';
+        } else if (input.contains(" / ")) {
+            operator = '/';
+        } else {
+            throw new Exception("Строка не является математической операцией");
+        }
+
+        String[] parts = input.split(" \\" + operator + " ");
+        if (parts.length != 2) {
+            throw new Exception("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
+
+        int num1,num2;
+        try {
+            num1 = Integer.parseInt(parts[0].trim());
+            num2 = Integer.parseInt(parts[1].trim());
+        } catch (NumberFormatException e) {
+            throw new Exception("Операнд не является целым числом");
+        }
+
+        if (num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10) {
+            throw new Exception("Числа должны быть от 1 до 10 включительно");
+        }
+        int result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    throw new Exception("Деление на ноль невозможно");
+                }
+                result = num1 / num2;
+                break;
+            default:
+                throw new Exception("Неизвестная ошибка");
+        }
+        return String.valueOf(result);
     }
 }
